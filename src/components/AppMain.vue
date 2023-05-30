cf2721d333139d388a0a00b080c10ad0b5339a38
Marco Rinaldi <marcorinaldi98@libero.it>
2023-05-30 15:47:25 +0200

    Creato componente project card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,17 +1,23 @@
 <script>
 import axios from 'axios';
 
+import ProjectCard from './ProjectCard.vue';
+
 export default {
     name: "AppMain",
     data() {
         return {
             projects: [],
-            baseUrl: 'http://localhost:8000',
+            baseUrl: 'http://localhost:8000'
         }
     },
+    components: {
+        ProjectCard
+    },
     methods: {
+        // funzione per accedere tramite axios all'array dei projects creata in laravel
         getProjects() {
-            axios.get(`http://localhost:8000/api/projects`)
+            axios.get(`${this.baseUrl}/api/projects`)
                 .then(response => {
                     console.log(response);
                     this.projects = response.data.results;
@@ -29,18 +35,7 @@ export default {
         <div class="row">
 
             <div class="col-4" v-for="project in projects">
-                <div class="card">
-                    <img v-if="project.image" :src="`${this.baseUrl}/storage/${project.image}`" class="card-img-top">
-                    <img v-else
-                        src="https://media.istockphoto.com/id/887464786/it/vettoriale/nessuna-fotocamera-consentita-segno-icona-piatta-in-cerchio-barrato-rosso-vettore.jpg?s=612x612&w=0&k=20&c=kmSQFxPBSqRjO9hTIgyYHxj82GYOB6kGTPc52kNQ_ug="
-                        class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ project.title }}</h5>
-                        <h5>{{ project.technology?.name }}</h5>
-                        <p class="card-text">{{ project.description }}</p>
-                        <a href="#" class="btn btn-primary">VEDI</a>
-                    </div>
-                </div>
+                <ProjectCard :item="project"></ProjectCard>
             </div>
 
         </div>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    name: "ProjectCard",
+    props: {
+        item: Object
+    },
+    data() {
+        return {
+            descriptionMaxLength: 150
+        }
+    },
+    methods: {
+        //funzione per delimitare i testi lunghi
+        truncateText(text) {
+            if (text && text.length > this.descriptionMaxLength) {
+                return text.substr(0, this.descriptionMaxLength) + '...';
+            }
+            return text;
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card my-2">
+        <img v-if="this.item.image" :src="`${this.baseUrl}/storage/${this.item.image}`" class="card-img-top">
+        <img v-else
+            src="https://media.istockphoto.com/id/887464786/it/vettoriale/nessuna-fotocamera-consentita-segno-icona-piatta-in-cerchio-barrato-rosso-vettore.jpg?s=612x612&w=0&k=20&c=kmSQFxPBSqRjO9hTIgyYHxj82GYOB6kGTPc52kNQ_ug="
+            class="card-img-top">
+        <div class="card-body">
+            <h5 class="card-title">{{ this.item.title }}</h5>
+            <h5>{{ this.item.technology?.name }}</h5>
+            <p class="card-text">{{ this.truncateText(this.item.description) }}</p>
+            <a href="#" class="btn btn-primary">VEDI</a>
+        </div>
+    </div>
+</template>
+
+<style lang="scss"></style>
